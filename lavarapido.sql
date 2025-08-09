4417501867e43d726535f2bf8cc88e4d9cc0b60b
Rihanna C. Arantes <carolinaarantes06@gmail.com>
2025-08-09 19:03:55 -0300

    Add files via upload
    lavarapidos

--- lavarapido.sql
+++ lavarapido.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 06-Ago-2025 às 17:45
--- Versão do servidor: 10.4.22-MariaDB
--- versão do PHP: 8.1.2
+-- Tempo de geração: 10/08/2025 às 00:00
+-- Versão do servidor: 10.4.32-MariaDB
+-- Versão do PHP: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -20,131 +20,253 @@ SET time_zone = "+00:00";
 --
 -- Banco de dados: `lavarapido`
 --
-CREATE DATABASE IF NOT EXISTS `lavarapido` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-USE `lavarapido`;
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `idcliente`
+-- Estrutura para tabela `atendimento`
 --
 
-CREATE TABLE `idcliente` (
-  `cliente` int(11) NOT NULL,
+CREATE TABLE `atendimento` (
+  `codos` int(11) NOT NULL,
+  `codcarro` int(11) DEFAULT NULL,
+  `totalgeral` decimal(10,2) DEFAULT NULL,
+  `data` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `atendimento_servico`
+--
+
+CREATE TABLE `atendimento_servico` (
+  `codatendimentoservico` int(11) DEFAULT NULL,
+  `codos` int(11) DEFAULT NULL,
+  `codservico` int(11) DEFAULT NULL,
+  `qtde` int(11) DEFAULT NULL,
+  `totalserv` decimal(10,2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `carro`
+--
+
+CREATE TABLE `carro` (
+  `codcarro` int(11) NOT NULL,
+  `placa` varchar(10) DEFAULT NULL,
+  `ano` int(11) DEFAULT NULL,
+  `codcliente` int(11) DEFAULT NULL,
+  `codmodelo` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `cliente`
+--
+
+CREATE TABLE `cliente` (
+  `codcliente` int(11) NOT NULL,
   `nome` varchar(100) DEFAULT NULL,
-  `datadenascimento` date DEFAULT NULL,
+  `datanascimento` date DEFAULT NULL,
   `telefone` varchar(20) DEFAULT NULL,
   `email` varchar(100) DEFAULT NULL,
-  `idendereco` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `codendereco` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `idendereco`
+-- Estrutura para tabela `endereco`
 --
 
-CREATE TABLE `idendereco` (
-  `endereco` int(11) NOT NULL,
+CREATE TABLE `endereco` (
+  `codendereco` int(11) NOT NULL,
   `rua` varchar(100) DEFAULT NULL,
   `num` varchar(10) DEFAULT NULL,
   `bairro` varchar(50) DEFAULT NULL,
   `cidade` varchar(50) DEFAULT NULL,
   `estado` char(2) DEFAULT NULL,
   `cep` varchar(10) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `idmodelo`
---
-
-CREATE TABLE `idmodelo` (
-  `idmodelo` int(11) NOT NULL,
-  `modelo` varchar(255) DEFAULT NULL,
-  `idmarca` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `marca`
+-- Estrutura para tabela `marca`
 --
 
 CREATE TABLE `marca` (
-  `idmarca` int(11) NOT NULL,
-  `marca` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `codmarca` int(11) NOT NULL,
+  `marca` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `modelo`
+--
+
+CREATE TABLE `modelo` (
+  `codmodelo` int(11) NOT NULL,
+  `modelo` varchar(50) DEFAULT NULL,
+  `codmarca` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `servico`
+--
+
+CREATE TABLE `servico` (
+  `codservico` int(11) NOT NULL,
+  `servico` varchar(100) DEFAULT NULL,
+  `valor` decimal(10,2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Índices para tabelas despejadas
 --
 
 --
--- Índices para tabela `idcliente`
+-- Índices de tabela `atendimento`
 --
-ALTER TABLE `idcliente`
-  ADD PRIMARY KEY (`cliente`),
-  ADD UNIQUE KEY `email` (`email`),
-  ADD KEY `idendereco` (`idendereco`);
+ALTER TABLE `atendimento`
+  ADD PRIMARY KEY (`codos`),
+  ADD KEY `codcarro` (`codcarro`);
 
 --
--- Índices para tabela `idendereco`
+-- Índices de tabela `atendimento_servico`
 --
-ALTER TABLE `idendereco`
-  ADD PRIMARY KEY (`endereco`);
+ALTER TABLE `atendimento_servico`
+  ADD KEY `codos` (`codos`),
+  ADD KEY `codservico` (`codservico`);
 
 --
--- Índices para tabela `idmodelo`
+-- Índices de tabela `carro`
 --
-ALTER TABLE `idmodelo`
-  ADD PRIMARY KEY (`idmodelo`),
-  ADD KEY `idmarca` (`idmarca`);
+ALTER TABLE `carro`
+  ADD PRIMARY KEY (`codcarro`),
+  ADD KEY `codcliente` (`codcliente`),
+  ADD KEY `codmodelo` (`codmodelo`);
 
 --
--- Índices para tabela `marca`
+-- Índices de tabela `cliente`
+--
+ALTER TABLE `cliente`
+  ADD PRIMARY KEY (`codcliente`),
+  ADD KEY `codendereco` (`codendereco`);
+
+--
+-- Índices de tabela `endereco`
+--
+ALTER TABLE `endereco`
+  ADD PRIMARY KEY (`codendereco`);
+
+--
+-- Índices de tabela `marca`
 --
 ALTER TABLE `marca`
-  ADD PRIMARY KEY (`idmarca`);
+  ADD PRIMARY KEY (`codmarca`);
 
 --
--- AUTO_INCREMENT de tabelas despejadas
+-- Índices de tabela `modelo`
+--
+ALTER TABLE `modelo`
+  ADD PRIMARY KEY (`codmodelo`),
+  ADD KEY `codmarca` (`codmarca`);
+
+--
+-- Índices de tabela `servico`
+--
+ALTER TABLE `servico`
+  ADD PRIMARY KEY (`codservico`);
+
+--
+-- AUTO_INCREMENT para tabelas despejadas
 --
 
 --
--- AUTO_INCREMENT de tabela `idcliente`
+-- AUTO_INCREMENT de tabela `atendimento`
 --
-ALTER TABLE `idcliente`
-  MODIFY `cliente` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `atendimento`
+  MODIFY `codos` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT de tabela `idmodelo`
+-- AUTO_INCREMENT de tabela `carro`
 --
-ALTER TABLE `idmodelo`
-  MODIFY `idmodelo` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `carro`
+  MODIFY `codcarro` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `cliente`
+--
+ALTER TABLE `cliente`
+  MODIFY `codcliente` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `endereco`
+--
+ALTER TABLE `endereco`
+  MODIFY `codendereco` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de tabela `marca`
 --
 ALTER TABLE `marca`
-  MODIFY `idmarca` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `codmarca` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- Restrições para despejos de tabelas
+-- AUTO_INCREMENT de tabela `modelo`
+--
+ALTER TABLE `modelo`
+  MODIFY `codmodelo` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `servico`
+--
+ALTER TABLE `servico`
+  MODIFY `codservico` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restrições para tabelas despejadas
 --
 
 --
--- Limitadores para a tabela `idcliente`
+-- Restrições para tabelas `atendimento`
 --
-ALTER TABLE `idcliente`
-  ADD CONSTRAINT `idcliente_ibfk_1` FOREIGN KEY (`idendereco`) REFERENCES `idendereco` (`endereco`);
+ALTER TABLE `atendimento`
+  ADD CONSTRAINT `atendimento_ibfk_1` FOREIGN KEY (`codcarro`) REFERENCES `carro` (`codcarro`);
 
 --
--- Limitadores para a tabela `idmodelo`
+-- Restrições para tabelas `atendimento_servico`
 --
-ALTER TABLE `idmodelo`
-  ADD CONSTRAINT `idmodelo_ibfk_1` FOREIGN KEY (`idmarca`) REFERENCES `marca` (`idmarca`);
+ALTER TABLE `atendimento_servico`
+  ADD CONSTRAINT `atendimento_servico_ibfk_1` FOREIGN KEY (`codos`) REFERENCES `atendimento` (`codos`),
+  ADD CONSTRAINT `atendimento_servico_ibfk_2` FOREIGN KEY (`codservico`) REFERENCES `servico` (`codservico`);
+
+--
+-- Restrições para tabelas `carro`
+--
+ALTER TABLE `carro`
+  ADD CONSTRAINT `carro_ibfk_1` FOREIGN KEY (`codcliente`) REFERENCES `cliente` (`codcliente`),
+  ADD CONSTRAINT `carro_ibfk_2` FOREIGN KEY (`codmodelo`) REFERENCES `modelo` (`codmodelo`);
+
+--
+-- Restrições para tabelas `cliente`
+--
+ALTER TABLE `cliente`
+  ADD CONSTRAINT `cliente_ibfk_1` FOREIGN KEY (`codendereco`) REFERENCES `endereco` (`codendereco`);
+
+--
+-- Restrições para tabelas `modelo`
+--
+ALTER TABLE `modelo`
+  ADD CONSTRAINT `modelo_ibfk_1` FOREIGN KEY (`codmarca`) REFERENCES `marca` (`codmarca`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
